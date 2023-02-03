6ec8b7cd2f347e154f0e4b701c12ba620787880b
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-02-03 14:42:11 +0100

    Create construct

--- models/Movie.php
+++ models/Movie.php
@@ -5,6 +5,18 @@ class Movie
     public $bannerPath;
     public $title;
     public $year;
-    public $genre;
     public $duration;
+    public $genre;
+
+    public function __construct($_bannerPath, $_title, $_year, $_duration,  $_genre)
+    {
+        $this->bannerPath = $_bannerPath;
+        $this->title = $_title;
+        $this->year = $_year;
+        $this->duration = $_duration;
+        $this->genre = $_genre;
+    }
 }
+
+$gino = new Movie('#', 'blabla', '2003', '1:03h', 'fantasy'); //For test - To remove
+var_dump($gino); //For test - To remove
